2697794c4d02deabf2a550bb681d5bab37720f37
Dstrasel <siruku@gmail.com>
2016-12-03 16:36:49 -0500

    Testing request.

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, request, redirect
 import twilio.twiml
+from flask import request
+
+#request.headers()
 
 app = Flask(__name__)
 
@@ -8,9 +11,12 @@ def hello_monkey():
     """Respond to incoming calls with a simple text message."""
 
     resp = twilio.twiml.Response()
-    resp.message("Hello, Mobile Monkey")
+    # resp.message("Hello, Mobile Monkey")
+    resp.message(str(request))
     return str(resp)
 
 if __name__ == "__main__":
     app.run(debug=True)
 
+# https://ancient-harbor-62057.herokuapp.com/
+# https://demo.twilio.com/welcome/sms/reply/
