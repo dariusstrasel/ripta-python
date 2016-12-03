8788d7043e4ec0e931a471bb10354e29d0e5a5f6
Dstrasel <siruku@gmail.com>
2016-12-03 18:11:58 -0500

    commit

--- app.py
+++ app.py
@@ -13,12 +13,12 @@ def hello_monkey():
     resp = twilio.twiml.Response()
     # resp.message("Hello, Mobile Monkey")
     try:
-        response_data = request.form.to_dict()
+        response_data = request.form.to_dict()['Body']
         print(response_data)
     except Exception:
         response_data = Exception
 
-    resp.message(str(response_data['body']))
+    resp.message(str(response_data))
     return str(resp)
 
 if __name__ == "__main__":
@@ -28,3 +28,4 @@ if __name__ == "__main__":
 # https://demo.twilio.com/welcome/sms/reply/
 
 """ImmutableMultiDict([('NumMedia', u'0'), ('NumSegments', u'1'), ('FromZip', u'33016'), ('SmsMessageSid', u'SM6c7f8711cef645b067f55fb902bb4829'), ('Body', u'Test'), ('FromCountry', u'US'), ('To', u'+14012831818'), ('MessageSid', u'SM6c7f8711cef645b067f55fb902bb4829'), ('ApiVersion', u'2010-04-01'), ('FromState', u'FL'), ('SmsSid', u'SM6c7f8711cef645b067f55fb902bb4829'), ('ToState', u'RI'), ('ToCity', u'PROVIDENCE'), ('ToZip', u'02906'), ('ToCountry', u'US'), ('From', u'+13057074681'), ('SmsStatus', u'received'), ('AccountSid', u'AC78c549fa01a56be05531f28061f754f2'), ('FromCity', u'MIAMI')])"""
+
