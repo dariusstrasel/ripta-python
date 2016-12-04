3b7b8b63786f42b16baf1dc4237f956a8689ef14
Dstrasel <siruku@gmail.com>
2016-12-03 21:28:11 -0500

    Removed test code and stubs.

--- app.py
+++ app.py
@@ -1,31 +1,22 @@
 from flask import Flask, request, redirect
 import twilio.twiml
-from flask import request
 
-#request.headers()
 
 app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
-def hello_monkey():
-    """Respond to incoming calls with a simple text message."""
+def echo_input():
+    """Respond to incoming texts with a copy of the same text."""
 
     resp = twilio.twiml.Response()
-    # resp.message("Hello, Mobile Monkey")
     try:
         response_data = request.form.to_dict()['Body']
         print(response_data)
     except Exception:
-        response_data = Exception
+        response_data = Exception.__base__
 
     resp.message(str(response_data))
     return str(resp)
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-# https://ancient-harbor-62057.herokuapp.com/
-# https://demo.twilio.com/welcome/sms/reply/
-
-"""ImmutableMultiDict([('NumMedia', u'0'), ('NumSegments', u'1'), ('FromZip', u'33016'), ('SmsMessageSid', u'SM6c7f8711cef645b067f55fb902bb4829'), ('Body', u'Test'), ('FromCountry', u'US'), ('To', u'+14012831818'), ('MessageSid', u'SM6c7f8711cef645b067f55fb902bb4829'), ('ApiVersion', u'2010-04-01'), ('FromState', u'FL'), ('SmsSid', u'SM6c7f8711cef645b067f55fb902bb4829'), ('ToState', u'RI'), ('ToCity', u'PROVIDENCE'), ('ToZip', u'02906'), ('ToCountry', u'US'), ('From', u'+13057074681'), ('SmsStatus', u'received'), ('AccountSid', u'AC78c549fa01a56be05531f28061f754f2'), ('FromCity', u'MIAMI')])"""
-
--- send_request.py
+++ send_request.py
@@ -1,3 +1,17 @@
 import requests
 
-r = requests.post('http://127.0.0.1:5000')
+payload = {'Body': 'blank'}
+local_server = 'http://127.0.0.1:5000'
+live_server = 'https://ancient-harbor-62057.herokuapp.com/'
+
+def send_test_payload(url, payload):
+    try:
+        response = requests.post(url, data=payload).text
+    except Exception as ex:
+        template = "An exception of type {0} occured."
+        message = template.format(type(ex).__name__)
+        response = message
+    return response
+
+print(send_test_payload(local_server, payload))
+print(send_test_payload(live_server, payload))
