2d8c690a6b9451637dd4ce4e1f4bf96782c11ea0
Dstrasel <siruku@gmail.com>
2016-12-04 10:45:39 -0500

    Connected to RIPTACalculations module.

--- app.py
+++ app.py
@@ -4,18 +4,21 @@ import RIPTAStopCalculations
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=['GET', 'POST'])
 def echo_input():
     """Respond to incoming texts with a copy of the same text."""
 
     resp = twilio.twiml.Response()
     try:
-        response_data = request.form.to_dict()['Body']
+        response_data = int(request.form.to_dict()['Body'])
         # print(response_data)
     except Exception:
         response_data = Exception.__base__
 
-    resp.message(str(response_data))
+    stop_distance = RIPTAStopCalculations.smsReply(response_data)
+
+    resp.message(str(stop_distance))
     return str(resp)
 
 if __name__ == "__main__":
